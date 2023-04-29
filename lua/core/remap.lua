f9d7ea303de5ce04e55c7be3300df97057f01c60
Aske Lange <44546482+AskeLange@users.noreply.github.com>
2023-04-29 20:36:49 +0200

    feat: added more directory remaps

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,6 +1,11 @@
+ -- Map Leader
 vim.g.mapleader = " "
 
  -- General
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
 vim.keymap.set("n", "<leader>,", "<C-6>")
+vim.keymap.set("t", "<leader><ESC>", "<C-\\><C-n>")
+
+ -- File Explorer 
+vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 vim.keymap.set("n", "<leader>in", ":Ex ~/.config/nvim<CR>")
+vim.keymap.set("n", "<leader>re", ":Ex ~/Repositories<CR>")
