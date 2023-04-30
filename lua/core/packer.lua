3e1afcc0436d5964f36a6ea288d7ff5a96ea3503
Aske Lange <44546482+AskeLange@users.noreply.github.com>
2023-04-30 14:13:39 +0200

    feat: added prettier support

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,43 +1,48 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'nvim-lua/plenary.nvim'
-	use 'ThePrimeagen/harpoon'
-	use 'mbbill/undotree'
-	use 'tpope/vim-fugitive'
+    use 'wbthomason/packer.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use 'ThePrimeagen/harpoon'
+    use 'mbbill/undotree'
+    use 'tpope/vim-fugitive'
+    use('neovim/nvim-lspconfig')
+    use('jose-elias-alvarez/null-ls.nvim')
+    use('MunifTanjim/prettier.nvim')
 
-	use { 'rose-pine/neovim', as = 'rose-pine' }
-	use { 'nvim-telescope/telescope.nvim', tag = '0.1.1' }
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = function()
-			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-			ts_update()
-		end,
-	}
+    use { 'rose-pine/neovim', as = 'rose-pine' }
+    use { 'nvim-telescope/telescope.nvim', tag = '0.1.1' }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
 
-	use('nvim-treesitter/playground')
+    use('nvim-treesitter/playground')
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			run = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim', run = function() pcall(vim.cmd, 'MasonUpdate') end},
+            {'williamboman/mason-lspconfig.nvim'},
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
 
-	}
-}
+        }
+
+    }
+
+    use {
+        "nvim-telescope/telescope-file-browser.nvim",
+        requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    }
+
 end)
