7241e9316a9e8c3a8e7d2cd54e38cf10a59b4f9a
AskeLange <ala@limbo.works>
2023-05-05 15:30:54 +0200

    chore: updated repositories remap

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -8,5 +8,5 @@ vim.keymap.set("t", "<leader><ESC>", "<C-\\><C-n>")
  -- File Explorer 
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 vim.keymap.set("n", "<leader>in", ":Ex ~/.config/nvim<CR>:cd ~/.config/nvim<CR>")
-vim.keymap.set("n", "<leader>re", ":Ex ~/repos<CR>:cd ~/repos<CR>")
+vim.keymap.set("n", "<leader>re", ":Ex ~/Repositories<CR>:cd ~/Repositories<CR>")
 
